b7cd5a10bea006c2fcba5a8413e3a66b15f35e0f
Lukas <lukas@Lukass-iMac-2.local>
2020-09-22 15:49:18 +0200

    Passport installed

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/', function () {
 Route::resource('products', ProductController::class);
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
