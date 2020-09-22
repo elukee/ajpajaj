4522f2b9433f44ef7b5a9cc0f208832e884dc5d1
Lukas <lukas@Lukass-iMac-2.local>
2020-09-22 09:46:56 +0200

    rebuild structure again

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Model\Review;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -41,7 +41,7 @@ class ReviewController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Model\Review  $review
+     * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
     public function show(Review $review)
@@ -52,7 +52,7 @@ class ReviewController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Model\Review  $review
+     * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
     public function edit(Review $review)
@@ -64,7 +64,7 @@ class ReviewController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Model\Review  $review
+     * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Review $review)
@@ -75,7 +75,7 @@ class ReviewController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Model\Review  $review
+     * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
     public function destroy(Review $review)
